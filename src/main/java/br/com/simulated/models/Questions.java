76bb6882546659253a73a1d14a4953476f81e76f
Dernival Liandro <liandro.sys@gmail.com>
2019-08-15 00:58:00 -0300

    Adição das funções de inserir e deletar questões.

---
+++ src/main/java/br/com/simulated/models/Questions.java
@@ -0,0 +1,47 @@
+package br.com.simulated.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "questions")
+public class Questions {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "question", length = 20, nullable = false)
+	private String question;
+	
+	@Column(name = "reply", length = 200, nullable = false)
+	private String reply;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getQuestion() {
+		return question;
+	}
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	public String getReply() {
+		return reply;
+	}
+	public void setReply(String reply) {
+		this.reply = reply;
+	}
+	
+	
+
+}
